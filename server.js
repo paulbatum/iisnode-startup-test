87fd5330fae9ed5c803e56372df4837fa06f8499
Paul Batum <paul.batum@gmail.com>
2014-07-09 18:27:06 -0700

    added so many modules

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 var http = require('http');
 
+require('async');
+require('azure');
+require('dpush');
+require('mpns');
+require('oauth');
+require('pusher');
+require('request');
+require('sax');
+require('sendgrid');
+require('twilio');
+require('underscore');
+require('underscore.string');
+require('wns');
+require('xml2js');
+require('xmlbuilder');
+
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end('Hello, world!');
     
 }).listen(process.env.PORT || 8080);
+
+console.log("listening...");
